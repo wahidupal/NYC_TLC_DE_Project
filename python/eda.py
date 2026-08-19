3db18a5e6caef25d105953c0e35fa47ccafb8ed2
wahidupal <83787424+wahidupal@users.noreply.github.com>
2026-08-19 11:41:08 +0200

    Update eda.py

--- python/eda.py
+++ python/eda.py
@@ -4,10 +4,10 @@ con = duckdb.connect()
 query = """
 SELECT
     payment_type,
-    COUNT(*) AS trips,
-    ROUND(AVG(trip_distance), 2) AS avg_distance,
-    ROUND(AVG(total_amount), 2) AS avg_total_amount,
-    ROUND(AVG(total_amount / NULLIF(trip_distance, 0)), 2) AS avg_dollars_per_mile
+    MIN(trip_distance) AS min_distance,
+    MAX(trip_distance) AS max_distance,
+    MIN(total_amount) AS min_total,
+    MAX(total_amount) AS max_total
 FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
 GROUP BY payment_type
 ORDER BY payment_type;
@@ -22,38 +22,14 @@ con.close()
 con = duckdb.connect()
 query = """
 SELECT
-    payment_type,
-    COUNT(*) AS trips,
-
-    ROUND(SUM(total_amount), 2) AS total_revenue,
-    ROUND(SUM(trip_distance), 2) AS total_miles,
-
-    ROUND(
-        SUM(total_amount) / NULLIF(SUM(trip_distance), 0),
-        2
-    ) AS revenue_per_mile
-
+    trip_distance,
+    total_amount,
+    total_amount / NULLIF(trip_distance, 0) AS dollars_per_mile
 FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
-GROUP BY payment_type
-ORDER BY payment_type;
-"""
-
-result = con.execute(query).fetchdf()
-
-print(result.to_string(index=False))
-
-con.close()
-
-con = duckdb.connect()
-query = """
-SELECT
-    payment_type,
-    ROUND(AVG(fare_amount), 2) AS avg_fare,
-    ROUND(AVG(total_amount), 2) AS avg_total,
-    ROUND(AVG(total_amount - fare_amount), 2) AS avg_difference
-FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
-GROUP BY payment_type
-ORDER BY payment_type;
+WHERE payment_type = 0
+  AND trip_distance > 0
+ORDER BY dollars_per_mile DESC
+LIMIT 20;
 """
 
 result = con.execute(query).fetchdf()
