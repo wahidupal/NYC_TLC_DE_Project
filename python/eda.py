a19086b63d98750922bc4e294e189f69e6fee186
wahidupal <83787424+wahidupal@users.noreply.github.com>
2026-08-21 16:23:07 +0200

    Update eda.py

--- python/eda.py
+++ python/eda.py
@@ -3,15 +3,33 @@ import duckdb
 con = duckdb.connect()
 query = """
 SELECT
-    COUNT(*) AS total_trips,
-    COUNT(*) FILTER (WHERE trip_distance > 20) AS over_20,
-    COUNT(*) FILTER (WHERE trip_distance > 50) AS over_50,
-    COUNT(*) FILTER (WHERE trip_distance > 100) AS over_100,
-    COUNT(*) FILTER (WHERE trip_distance > 20 AND trip_distance < 100) AS between_20_100,
-    COUNT(*) FILTER (WHERE trip_distance > 500) AS over_500,
-    COUNT(*) FILTER (WHERE trip_distance > 1000) AS over_1k,
-    COUNT(*) FILTER (WHERE trip_distance > 10000) AS over_10k,
-    COUNT(*) FILTER (WHERE trip_distance > 100000) AS over_100k
+    COUNT(*) FILTER (WHERE trip_distance < 20) AS below_20,
+    COUNT(*) FILTER (WHERE trip_distance >= 20 AND trip_distance < 50) AS between_20_50,
+    COUNT(*) FILTER (WHERE trip_distance >= 50 AND trip_distance < 100) AS between_50_100,
+    COUNT(*) FILTER (WHERE trip_distance >= 100 AND trip_distance < 500) AS between_100_500,
+    COUNT(*) FILTER (WHERE trip_distance >= 500 AND trip_distance < 1000) AS between_500_1000,
+    COUNT(*) FILTER (WHERE trip_distance >= 1000 AND trip_distance < 10000) AS between_1k_10k,
+    COUNT(*) FILTER (WHERE trip_distance >= 10000) AS over_10k,
+    AVG(
+        ROUND(
+            EXTRACT(EPOCH FROM (
+                tpep_dropoff_datetime - tpep_pickup_datetime
+            )) / 60,
+            2
+        )
+    ) AS avg_duration_minutes,
+    ROUND(
+        QUANTILE_CONT(
+            EXTRACT(EPOCH FROM (
+                tpep_dropoff_datetime - tpep_pickup_datetime
+            )) / 60,
+            0.50
+        ),
+        2
+    ) AS median_duration_minutes,
+    AVG (fare_amount) AS avg_fare_amount,
+    AVG (total_amount) AS avg_total_amount
+
 
 FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
 WHERE VendorID = 2;
@@ -25,39 +43,24 @@ con.close()
 
 con = duckdb.connect()
 query = """
-SELECT
-    COUNT(*) AS total_trips,
-    COUNT(*) FILTER (WHERE trip_distance > 20) AS over_20,
-    COUNT(*) FILTER (WHERE trip_distance > 50) AS over_50,
-    COUNT(*) FILTER (WHERE trip_distance > 100) AS over_100,
-    COUNT(*) FILTER (WHERE trip_distance > 20 AND trip_distance < 100) AS between_20_100,
-    COUNT(*) FILTER (WHERE trip_distance > 500) AS over_500,
-    COUNT(*) FILTER (WHERE trip_distance > 1000) AS over_1k,
-    COUNT(*) FILTER (WHERE trip_distance > 10000) AS over_10k,
-    COUNT(*) FILTER (WHERE trip_distance > 100000) AS over_100k
-
-FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet');
-"""
-
-result = con.execute(query).fetchdf()
-
-print(result.to_string(index=False))
-
-con.close()
-
-con = duckdb.connect()
-query = """
-SELECT
-    payment_type,
-    RatecodeID,
-    COUNT(*) AS trips,
-    MIN(trip_distance) AS min_distance,
-    MAX(trip_distance) AS max_distance
+SELECT 
+Distance_Bucket,
+COUNT (*)
+FROM (SELECT
+    CASE 
+        WHEN trip_distance < 20 THEN 'below_20'
+        WHEN trip_distance >= 20 AND trip_distance < 50 THEN 'between_20_50'
+        WHEN trip_distance >= 50 AND trip_distance < 100 THEN 'between_50_100'
+        WHEN trip_distance >= 100 AND trip_distance < 500 THEN 'between_100_500'
+        WHEN trip_distance >= 500 AND trip_distance < 1000 THEN 'between_500_1000'
+        WHEN trip_distance >= 1000 AND trip_distance < 10000 THEN 'between_1k_10k'
+        ELSE '10k+'
+    END AS Distance_Bucket
 FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
-WHERE VendorID = 2
-  AND trip_distance > 500
-GROUP BY payment_type, RatecodeID
-ORDER BY trips DESC;
+WHERE VendorID = 2) t
+
+GROUP BY Distance_Bucket
+LIMIT 20;
 """
 
 result = con.execute(query).fetchdf()
@@ -65,4 +68,17 @@ result = con.execute(query).fetchdf()
 print(result.to_string(index=False))
 
 con.close()
+
+# con = duckdb.connect()
+# query = """
+# SELECT *
+# FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
+# LIMIT 1
+# """
+
+# result = con.execute(query).fetchdf()
+
+# print(result.to_string(index=False))
+
+# con.close()
 
