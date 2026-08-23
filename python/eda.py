d7d80130a30ffc36217d88253a3556332a6f2da6
wahidupal <83787424+wahidupal@users.noreply.github.com>
2026-08-23 22:12:58 +0200

    Update eda.py

--- python/eda.py
+++ python/eda.py
@@ -1,44 +1,43 @@
 import duckdb
 
-con = duckdb.connect()
-query = """
-SELECT
-    VendorID,
-    payment_type,
-    trip_distance,
-    fare_amount,
-    total_amount,
-    tpep_pickup_datetime,
-    tpep_dropoff_datetime,
-    PULocationID,
-    DOLocationID
-FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
-WHERE VendorID = 7
-  AND tpep_dropoff_datetime = tpep_pickup_datetime
-LIMIT 20;
-"""
+# con = duckdb.connect()
+# query = """
+# SELECT
+#     VendorID,
+#     payment_type,
+#     trip_distance,
+#     fare_amount,
+#     total_amount,
+#     tpep_pickup_datetime,
+#     tpep_dropoff_datetime,
+#     PULocationID,
+#     DOLocationID
+# FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
+# WHERE VendorID = 7
+#   AND tpep_dropoff_datetime = tpep_pickup_datetime
+# LIMIT 20;
+# """
 
-result = con.execute(query).fetchdf()
+# result = con.execute(query).fetchdf()
 
-print(result.to_string(index=False))
+# print(result.to_string(index=False))
 
-con.close()
+# con.close()
 
 con = duckdb.connect()
 query = """
 SELECT
     COUNT(*) AS total_trips,
     COUNT(*) FILTER (
-        WHERE tpep_dropoff_datetime = tpep_pickup_datetime
-    ) AS zero_duration_trips,
+        WHERE congestion_surcharge IS NULL
+    ) AS congestion_nulls,
     COUNT(*) FILTER (
-        WHERE tpep_dropoff_datetime > tpep_pickup_datetime
-    ) AS positive_duration_trips,
+        WHERE Airport_fee IS NULL
+    ) AS airport_nulls,
     COUNT(*) FILTER (
-        WHERE tpep_dropoff_datetime < tpep_pickup_datetime
-    ) AS negative_duration_trips
-FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
-WHERE VendorID = 7;
+        WHERE cbd_congestion_fee IS NULL
+    ) AS cbd_nulls
+FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet');
 """
 
 result = con.execute(query).fetchdf()
@@ -49,14 +48,13 @@ con.close()
 
 con = duckdb.connect()
 query = """
-SELECT
-    MIN(tpep_pickup_datetime) AS first_pickup,
-    MAX(tpep_pickup_datetime) AS last_pickup,
-    COUNT(DISTINCT CAST(tpep_pickup_datetime AS DATE)) AS active_days,
-    COUNT(DISTINCT PULocationID) AS pickup_zones,
-    COUNT(DISTINCT DOLocationID) AS dropoff_zones
+SELECT 
+payment_type,
+COUNT (*) AS mismatched_trips
 FROM read_parquet('../data/raw/yellow_tripdata_2026-01.parquet')
-WHERE VendorID = 7;
+WHERE fare_amount + extra + mta_tax + tip_amount + tolls_amount + improvement_surcharge + congestion_surcharge + Airport_fee + cbd_congestion_fee != total_amount
+GROUP BY payment_type
+ORDER BY mismatched_trips DESC;
 """
 
 result = con.execute(query).fetchdf()
