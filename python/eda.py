b724a4464765372244a81f89404ba3dbbccb1100
wahidupal <83787424+wahidupal@users.noreply.github.com>
2026-08-25 22:08:33 +0200

    Update eda.py

--- python/eda.py
+++ python/eda.py
@@ -2,9 +2,13 @@ import duckdb
 
 con = duckdb.connect()
 query = """
-SELECT *
+SELECT
+    PUlocationID,
+    COUNT(*) AS trip_count
 FROM read_parquet('../data/raw/fhv_tripdata_2026-01.parquet')
-WHERE dropOff_datetime < pickup_datetime;
+GROUP BY PUlocationID
+ORDER BY trip_count DESC
+LIMIT 20;
 """
 
 result = con.execute(query).fetchdf()
@@ -13,18 +17,22 @@ print(result.to_string(index=False))
 
 con.close()
 
-# con = duckdb.connect()
-# query = """
-# SUMMARIZE
-# SELECT *
-# FROM read_parquet('../data/raw/fhvhv_tripdata_2026-01.parquet');
-# """
+con = duckdb.connect()
+query = """
+SELECT
+    DOlocationID,
+    COUNT(*) AS trip_count
+FROM read_parquet('../data/raw/fhv_tripdata_2026-01.parquet')
+GROUP BY DOlocationID
+ORDER BY trip_count DESC
+LIMIT 20;
+"""
 
-# result = con.execute(query).fetchdf()
+result = con.execute(query).fetchdf()
 
-# print(result.to_string(index=False))
+print(result.to_string(index=False))
 
-# con.close()
+con.close()
 
 # con = duckdb.connect()
 # query = """
